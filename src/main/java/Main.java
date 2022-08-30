e9d02e4bcd0997aebe662811c5eade311bd5bfdb
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-08-31 00:03:04 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 class Main {
+  
+  public static void testBasic() {
+    BankAccount acct1 = new BankAccount(0);
+    BankAccount acct2 = new BankAccount(0);
+    double million = 1.0e+6;
+    double billion = Math.pow(10, 9);
+    acct1.deposit((int)million);
+    acct2.deposit((int)billion);
+    acct1.withdraw((int)1.0e+5);    
+    acct2.withdraw(1);    
+    System.out.println(acct1);
+    System.out.println(acct2);
+  }
+
+  public static void testTransfer() {
+    boolean success = false;
+    System.out.println("Success = " + success);
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    testBasic();
+    testTransfer();
   }
 }
